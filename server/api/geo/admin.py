bb356b791f2aee07f642e3ebca9b648a7fdeeaa6
newbie-alfi <aleksandr20021008@gmail.com>
2022-11-28 09:09:31 +0300

    feat(server): added models to admin panel

--- server/api/geo/admin.py
+++ server/api/geo/admin.py
@@ -1,5 +1,8 @@
-from django.contrib.gis import admin
+from django.contrib.gis import admin as gis_admin
+from django.contrib import admin
+from .models import WorldBorder, Chronology, Region, Event
 
-from .models import WorldBorder
-
-admin.site.register(WorldBorder, admin.GeoModelAdmin)
+admin.site.register(WorldBorder, gis_admin.GeoModelAdmin)
+admin.site.register(Chronology, admin.ModelAdmin)
+admin.site.register(Region, admin.ModelAdmin)
+admin.site.register(Event, admin.ModelAdmin)
