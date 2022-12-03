bdf06c9f14f94ad6fc47288c233c20830ed2485d
newbie-alfi <aleksandr20021008@gmail.com>
2022-12-03 14:38:17 +0300

    fix(server): deleted useless route

--- server/config/urls.py
+++ server/config/urls.py
@@ -14,13 +14,20 @@ app_name = "chronology"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/token/", TokenObtainPairView.as_view(),  # type: ignore
-         name="token_obtain_pair"),
-    path("api/v1/token/refresh/", TokenRefreshView.as_view(),  # type: ignore
-         name="token_refresh"),
-    path("api/v1/token/verify/", TokenVerifyView.as_view(),  # type: ignore
-         name="token_verify"),
+    path(
+        "api/v1/token/",
+        TokenObtainPairView.as_view(),  # type: ignore
+        name="token_obtain_pair",
+    ),
+    path(
+        "api/v1/token/refresh/",
+        TokenRefreshView.as_view(),  # type: ignore
+        name="token_refresh",
+    ),
+    path(
+        "api/v1/token/verify/",
+        TokenVerifyView.as_view(),  # type: ignore
+        name="token_verify",
+    ),
     path("api/v1/", include(routers.urlpatterns)),
-    path("api/v1/token/verify/", TokenVerifyView.as_view(),  # type: ignore
-         name="token_verify"),
 ]
