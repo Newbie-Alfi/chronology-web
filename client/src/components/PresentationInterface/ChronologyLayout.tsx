ace60044e40cb5391ff2f60799da400ada7659b6
newbie-alfi <aleksandr20021008@gmail.com>
2022-12-09 01:30:59 +0300

    fix(client): refactoring

--- client/src/components/PresentationInterface/ChronologyLayout.tsx
+++ client/src/components/PresentationInterface/ChronologyLayout.tsx
@@ -1,6 +1,5 @@
 import { useMemo } from "react";
 import { observer } from "mobx-react-lite";
-import { useMap } from "../../hooks/useStores";
 import { useParams } from "react-router-dom";
 import { PresentationContext } from "../../contexts/PresentationContext";
 import { TimelineStore } from "../../store/timeline";
@@ -11,30 +10,29 @@ import { Loader } from "../common/Loader/Loader";
 export const PresentationInterface = observer(() => {
   // Эта страница доступна только если есть chronologyId в url
   const { chronologyId } = useParams() as { chronologyId: string };
-  const { map } = useMap();
   const { timeline } = useMemo(
     () => new TimelineStore(chronologyId),
     [chronologyId]
   );
 
-  const isLoading: boolean = timeline?.state === "pending" || map.loaded();
-
   return (
     <>
-      {isLoading && <Loader />}
-      {timeline?.state === "fulfilled" && (
-        <PresentationContext.Provider
-          value={{
-            presentationStore: new PresentationStore(
-              Number(chronologyId),
-              timeline.value.data.results[0].date,
-              timeline.value.data.results
-            ),
-          }}
-        >
-          <TopInterface></TopInterface>
-        </PresentationContext.Provider>
-      )}
+      {timeline?.case({
+        fulfilled: (v) => (
+          <PresentationContext.Provider
+            value={{
+              presentationStore: new PresentationStore(
+                Number(chronologyId),
+                v.data.results[0].date,
+                v.data.results
+              ),
+            }}
+          >
+            <TopInterface></TopInterface>
+          </PresentationContext.Provider>
+        ),
+        pending: () => <Loader />,
+      })}
     </>
   );
 });
