df47a0e89ce8be7fb1a7ee1111d1a965a6baced2
newbie-alfi <aleksandr20021008@gmail.com>
2022-12-09 01:07:04 +0300

    feat(client): added loader component

---
+++ client/src/components/common/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import { CircularProgress } from "@mui/material";
+import { CSSProperties, FC } from "react";
+
+import "./style.scss";
+
+interface ILoaderProps {
+  children?: React.ReactNode;
+  style?: CSSProperties;
+}
+
+export const Loader: FC<ILoaderProps> = ({
+  style,
+  children = <CircularProgress />,
+}) => (
+  <div style={style} className="full-size center">
+    {children}
+  </div>
+);
